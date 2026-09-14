3680df81a44cfaf6bac8bfeaf924482b0de53a80
Sumaiyya Zunairah <165111912+Sumaiyya-Zunairah@users.noreply.github.com>
2026-09-13 23:10:21 -0700

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -3,5 +3,5 @@ class Solution(object):
     def lengthOfLastWord(self, s):
         words = s.split()
         last_word = words[-1]
-        return len(last_word)
+        return len(last_word) 
     
